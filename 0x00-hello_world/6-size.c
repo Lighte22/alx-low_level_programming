b824ab919c090716cf681d93ec93bbcefe345424
lighte22 <enyilight@gmail.com>
2022-07-04 18:13:38 -0700

    My 6-size.c task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 /**
- * main - Write a C program that prints the size of various types,
- * on the computer it is compiled and run on.
+ * main - entry point
  *
  * Return: always 0
  */
 int main(void)
 {
-
-	printf("Size of a char: %lu byte(s)\n";
-	printf("Size of an int: %lu byte(s)\n";
-	printf("Size of a long int: %lu byte(s)\n";
-	printf("Size of a long long int: %lu byte(s)\n";
-	printf("Size of a float: %lu byte(s)\n";
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(d));
 	return (0);
 }
 
