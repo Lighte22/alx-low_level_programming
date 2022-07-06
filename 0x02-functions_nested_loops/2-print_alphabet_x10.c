68f99acb8b0542f24754078c67272d90782382fe
lighte22 <enyilight@gmail.com>
2022-07-06 16:07:04 -0700

    Nested loops task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,11 @@
 void print_alphabet_x10(void)
 
 {
+	char i;
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++);
+	for (i = 0; i < 10; i++)
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
 }
