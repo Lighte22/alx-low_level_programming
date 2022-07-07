7bcea9185d91a2611bae4e6f3f82d9669bc58968
Lighte22 <enyilight@gmail.com>
2022-07-07 23:17:40 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,13 @@
 
 /**
 * main - used to tests function that prints if integer is positive or negative
-* Return: 0
+* Return: alway 0
 */
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or_negative(i);
+		positive_or_negative(i);
 	return (0);
 }
