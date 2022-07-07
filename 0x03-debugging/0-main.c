ea35fdf33ffea352ca9e2e0a6cee5ddbb6b78689
Lighte22 <enyilight@gmail.com>
2022-07-07 23:21:16 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* main - used to tests function that prints if integer is positive or negative
+* main - entry point
+* Reason:used to tests function that prints if integer is positive or negative
 * Return: alway 0
 */
 int main(void)
