ec69ea92d3ffae2f581a5dd7f2f7a4dc89ecf0b4
Lighte22 <107030999+Lighte22@users.noreply.github.com>
2022-07-08 00:17:47 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,13 @@
 #include "main.h"
-/**
- * _islower - returns 1 if lowercase and 0 otherwise
- * @c:takes in a character
- * Return: 0 for uppercase, 1 for lowercase
- */
-int _islower(int c)
-{
-	int('0');
-	char ('i');
 
-	if (c >= 'a' && c <= 'z')
-		return (i);
-	else
-		return (0);
+/**
+* add -> adds two number
+* @x : param1
+* @y : param2
+* Return: x + y
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
 }
