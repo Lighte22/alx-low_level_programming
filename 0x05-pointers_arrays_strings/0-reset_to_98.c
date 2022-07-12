18a6555edacd3f20e3de5c01933defe528ef0735
Lighte22 <enyilight@gmail.com>
2022-07-12 19:41:30 +0100

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set the int to 98
+ * @n: shows the int to check
+ * owned by Lighte22
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+if (n)
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This a readme file for pointers, arrays and string.
