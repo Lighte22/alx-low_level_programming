44320606d9383675017b5b8e7c9e8ab8a8c5f4ad
Lighte22 <enyilight@gmail.com>
2022-07-15 16:27:46 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,22 +2,22 @@
 
 /**
 * reverse_array -> reversing an array
-* @a: array itself
+* @a: array a
 * @n: an element of an array
 * owned by Lighte22
 */
 void reverse_array(int *a, int n)
 {
-	int *j, i, aux, k;
+	int *p, i, aux, k;
 
 	j = a;
 	for (i = 0; i < n; i++)
-		j++;
+		p++;
 	for (k = 0; k < i / 2; k++)
 	{
 		aux = a[k];
 		a[k] = *j;
-		*j = aux;
-		j--;
+		*p = aux;
+		p--;
 	}
 }
