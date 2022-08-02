4d033ce99a4add1abd76a37c0872872dced0d483
Lighte22 <107030999+Lighte22@users.noreply.github.com>
2022-08-02 04:57:42 +0100

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef "0-object_like_macro.h"
-#define "0-object_like_macro.h"
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
 
-    #define s =1024;
+#define SIZE 1024
 
-#endif
+#endif /* _0_OBJECT_LIKE_MACRO_H_ */
