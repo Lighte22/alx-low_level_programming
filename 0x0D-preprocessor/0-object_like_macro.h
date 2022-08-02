77c6a93e9b4b80c5fccff3caaa114d7fc0a018ec
Lighte22 <107030999+Lighte22@users.noreply.github.com>
2022-08-02 04:53:55 +0100

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,19 +1,7 @@
+#ifndef "0-object_like_macro.h"
 #include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-      int s;
-
-    s = 98 + 1024;
-    ("%d\n", 1024);
+    s =1024;
+    ("%d, 1024);
     return (0);
 }
