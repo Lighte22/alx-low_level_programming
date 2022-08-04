6e2c8140dca50881605516cbd9c373b9a5b31be7
Lighte22 <107030999+Lighte22@users.noreply.github.com>
2022-08-04 18:51:26 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,14 +2,12 @@
 
 /**
  * print_name - a function that prints a name.
- * @name: the name given
- * @f: function of name
- *
- * Rteurn: void
+ * @name: pointer to the name to be printed
+ * @f: function pointer to call back a funtion
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == NULL || f == NULL)
 			return;
-		f(name);
 }
