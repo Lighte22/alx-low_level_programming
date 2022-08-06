5b85a5f0142e933e514ccb75fbb9932ce5edcc1f
Lighte22 <107030999+Lighte22@users.noreply.github.com>
2022-08-06 18:23:40 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,85 +1,23 @@
-#include "variadic_functions.h"
-#include <stdlib.h>
+#include "variadic_function.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 /**
- * print_int - prints int
- * @list: arguments from print_all
+ * c - prints character
  */
-void print_int(va_list list)
+
+char c(char a)
 {
-	printf("%d", va_arg(list, int));
+	if (a)
+		printf("%c", a);
 }
-
 /**
- * print_float - prints float
- * @list: arguments from print_all
- */
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
- * print_char - prints int
- * @list: arguments from print_all
- */
-void print_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-
-/**
- * print_str - prints string
- * @list: arguments from print_all
- */
-void print_str(va_list list)
-{
-	char *s = va_arg(list, char *);
-
-	s == NULL ? printf("(nil)") : printf("%s", s);
-
-}
-
-/**
- * print_all - prints any type
- * @format: arguments to print
+ * print_all - prints anything
+ *
+ * @format: format of the string to be printed
  */
 
 void print_all(const char * const format, ...)
 {
-va_list list;
-int i = 0, j = 0;
-char *sep = "";
-
-printTypeStruct printType[] = {
-	{ "i", print_int },
-	{ "f", print_float },
-	{ "c", print_char },
-	{ "s", print_str },
-	{NULL, NULL}
-};
-
-
-va_start(list, format);
-
-while (format && format[i])
-{
-	j = 0;
-	while (j < 4)
-	{
-		if (*printType[j].type == format[i])
-		{
-			printf("%s", sep);
-			printType[j].printer(list);
-			sep = ", ";
-			break;
-		}
-		j++;
-	}
-	i++;
-}
-
-printf("\n");
-va_end(list);
+	va_list
 }
